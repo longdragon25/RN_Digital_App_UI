edb5ec177fcac5cc256a934cc42a4f0165dbfd3f
longdragon25 <longdragon25112000xt@gmail.com>
2021-08-09 15:03:13 +0700

    commit 2

---
+++ screens/Scan.js
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native'
+
+import React from 'react'
+
+const Scan = () => {
+    return (
+        <View>
+            <Text>Scan</Text>
+        </View>
+    )
+}
+
+export default Scan
