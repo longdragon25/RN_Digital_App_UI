c830f6f839b308f9b321c578ce12a9347874baac
longdragon25 <longdragon25112000xt@gmail.com>
2021-08-09 20:29:44 +0700

    SignUp

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { LogBox } from "react-native";
+import React from 'react';
+import { SignUp } from "./screens";
+import Tabs from "./navigation/tabs";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const theme = {
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        border: "transparent",
+    },
+};
+
+const Stack = createStackNavigator();
+
+LogBox.ignoreAllLogs(true);
+const App = () => {
+    return (
+        <NavigationContainer theme={theme}>
+            <Stack.Navigator
+                screenOptions={{
+                    headerShown: false
+                }}
+                initialRouteName={'SignUp'}
+            >
+                <Stack.Screen name="SignUp" component={SignUp} />
+
+                {/* Tabs */}
+                <Stack.Screen name="Home" component={Tabs} />
+
+                {/* <Stack.Screen name="Scan" component={Scan} /> */}
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
